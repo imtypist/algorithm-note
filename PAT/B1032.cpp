60980b7293fedb6d44834f6c40071b5ed0777091
黄俊钦 <uestc_ccse@outlook.com>
2017-03-28 23:11:27 +0800

    挖掘机技术哪家强

---
+++ PAT/B1032.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int n,i;
+    int score,num;
+    scanf("%d",&n);
+    int* p = (int*)malloc(sizeof(int)*n);
+    memset(p,0,sizeof(int)*n);
+    for(i = 0;i < n;i++){
+        scanf("%d%d",&num,&score);
+        p[num] += score;
+    }
+    int MAX = -1;
+    for(i = 1;i <= n;i++){
+        if(p[i] > MAX){
+            MAX = p[i];
+            num = i;
+        }
+    }
+    printf("%d %d",num,MAX);
+    return 0;
+}
